d9372ac0816ac275b06120cb6fc517f59ca338d0
elyas ghasemi <egh1374@gmail.com>
2015-06-05 12:07:56 +0430

    add today birthdays list to response data

--- server/routes/birthday_sms.js
+++ server/routes/birthday_sms.js
@@ -9,14 +9,26 @@ module.exports = (function () {
         if(_.includes(req.user._permissions , "birthday_sms") || _.includes(req.user._permissions , "root")){
             db.birthday_sms.findOne({},{ text : true , enable : true , _id : false }).lean().exec(function(err,birthday_sms){
                 if(err) console.log(err);
-                else if(birthday_sms)
-                    res.json(birthday_sms);
+                else if(birthday_sms){
+                    birthday_sms.today = [] ;
+                    db.users.find({ birth_date : { $exists : true } },{ _id : false , birth_date : true ,mobile : true ,first_name : true , last_name : true }).lean().exec(function(err,users){
+                        users.forEach(function(user){
+                            var birth_date = new Date(user.birth_date);
+                            var date = new Date();
+                            if(birth_date.getMonth() == date.getMonth() && date.getDate() == birth_date.getDate() ){
+                                birthday_sms.today.push({ first_name: user.first_name , last_name : user.last_name });
+                            }
+                        });
+                        res.json(birthday_sms);
+                    });
+                }  
             });
         } else next();
     };
 
     _return.post = function (req, res ,next) {
-        if(req.user && req.body && req.body.text && (_.includes(req.user._permissions , "birthday_sms") || _.includes(req.user._permissions , "root")) ){
+        if(req.user && req.body && req.body.text && 
+            (_.includes(req.user._permissions , "birthday_sms") || _.includes(req.user._permissions , "root")) ){
             db.birthday_sms.findOne(function(err,birthday_sms){
                 birthday_sms.text = req.body.text ;
                 if(req.body.enable) birthday_sms.enable = true ;
