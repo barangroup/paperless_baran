7d248c46d93b339b3e42da181e16e4bd1f700eb5
elyas ghasemi <egh1374@gmail.com>
2015-06-17 16:13:57 +0430

    add permissions to post request

--- server/routes/send_sms.js
+++ server/routes/send_sms.js
@@ -23,28 +23,30 @@ module.exports = (function () {
     };
 
     _return.post = function (req, res ,next) {
-        console.open(req.body);
-        var data = req.body ;
-        if(data.text && data.contacts){
-            var query = {} ;
-            if(!(data.contacts.male && data.contacts.female)){
-                if(data.contacts.male){
-                    query.gender = true
-                } else if(data.contacts.male){
-                    query.gender = { $exists: false };
+        if(_.includes(req.user._permissions, "root") || _.includes(req.user._permissions, "sms_root")){
+            console.open(req.body);
+            var data = req.body ;
+            if(data.text && data.contacts){
+                var query = {} ;
+                if(!(data.contacts.male && data.contacts.female)){
+                    if(data.contacts.male){
+                        query.gender = true
+                    } else if(data.contacts.male){
+                        query.gender = { $exists: false };
+                    }
                 }
-            }
-            if(data.university) query.sadjad_uni_student = true ;
-            if(data.active) query.active = true ;
+                if(data.university) query.sadjad_uni_student = true ;
+                if(data.active) query.active = true ;
 
-            db.users.find(query).lean().exec(function(err,users){
-                if(err) console.log(err);
-                else if (users){
-                    res.json({ send : false , count : users.length }); // not completed yet
-                }
-            });
+                db.users.find(query).lean().exec(function(err,users){
+                    if(err) console.log(err);
+                    else if (users){
+                        res.json({ send : false , count : users.length }); // not completed yet
+                    }
+                });
 
-        } else res.json({ send : false , count : 0 });
+            } else res.json({ send : false , count : 0 });
+        }
     };
 
     return _return;
