d1e408758a6ad8d45516d38d206b4a9ee0f9ae68
elyas ghasemi <egh1374@gmail.com>
2015-06-18 00:47:40 +0430

    working on tasks part

---
+++ server/routes/tasks.js
@@ -0,0 +1,28 @@
+
+var db = require('mongo_schemas');
+var _ = require('lodash');
+
+module.exports = (function () {
+    var _return = {};
+
+    _return.get = function (req, res ,next) {
+    	if(_.includes(req.user._permissions, "root")){
+    		db.tasks.findOne({ name : "root" },{ __v : false }).lean().exec(function(err,task){
+                if(err) console.log(err);
+                else if(task){
+                    String.bfs(task._id,function (tree) {
+                        res.json(tree);  
+                    });
+                }
+    		});
+    	} else next();
+    };
+
+    _return.post = function (req, res ,next) {
+    	if(_.includes(req.user._permissions, "root")){
+
+    	} else next();
+    };
+
+    return _return;
+})();
