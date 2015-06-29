878e9a557451971c3a8494b62d8007b329d5aba3
elyas ghasemi <egh1374@gmail.com>
2015-06-30 01:55:57 +0430

    testing aggregate

--- server/testing.js
+++ server/testing.js
@@ -1,6 +1,27 @@
-
 var v = require('validator');
 var db = require('mongo_schemas');
 var _ = require('lodash');
 var encrypt = require('encrypt');
 
+
+db.users.aggregate({
+		$project: {
+			name: {
+				$concat: ["$first_name", " ", "$last_name"]
+			}
+		}
+	}, {
+		$match: {
+			name: 'something'
+		}
+	}, {
+		$limit: 5
+	}, {
+		$project: {
+			_id: "$_id"
+		}
+	},
+	function(err, rr) {
+		if (err) console.log(err);
+		// console.open(rr);
+	});
