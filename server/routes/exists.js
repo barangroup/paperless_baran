1a84db5953598c3e54bcd1519c0716534bef0c34
elyas ghasemi <egh1374@gmail.com>
2015-07-03 15:23:23 +0430

    add exists mobile route

---
+++ server/routes/exists.js
@@ -0,0 +1,34 @@
+var db = require('mongo_schemas');
+
+module.exports = (function() {
+    var _return = {};
+
+    _return.get = function(req, res, next) {
+        next();
+    };
+
+    _return.post = function(req, res, next) {
+        console.log('in');
+        if (req.body && req.body.mobile) {
+            db.users.findOne({
+                mobile: String.enc_mobile(req.body.mobile)
+            }, {
+                _id: true
+            }).lean().exec(function(err, result) {
+                if (err) {
+                    console.log(err);
+                } else if (result) {
+                    res.json({
+                        exists: true
+                    });
+                } else {
+                    res.json({
+                        exists: false
+                    });
+                };
+            });
+        } else next();
+    }
+
+    return _return;
+})();
