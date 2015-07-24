2023590396e6cfb671e764f3a707b400afd1f05f
elyas ghasemi <egh1374@gmail.com>
2015-07-24 23:22:48 +0430

    working on aggregate

--- server/testing.js
+++ server/testing.js
@@ -1,1 +1,26 @@
 var enc = require('date');
+
+// var db = require('mongo_schemas');
+
+// var q = db.dispatch.find().populate({
+// 	path: 'costs',
+// 	select: 'cost comment -_id'
+// });
+
+
+// q.aggregate(
+// 	[{
+// 		$group: {
+// 			_id: '$_id',
+// 			costs: {
+// 				$sum: "costs"
+// 			}
+// 		}
+// 	}]);
+
+
+// q.exec(
+// 	function(err, r) {
+// 		if (err) console.log(err);
+// 		else console.open(r);
+// 	});
