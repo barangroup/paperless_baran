ccbad19cad00d8b1f306df47b417b87828a5055f
elyas ghasemi <egh1374@gmail.com>
2015-07-24 23:23:37 +0430

    working on permissions

--- server/routes/mali.js
+++ server/routes/mali.js
@@ -5,40 +5,72 @@ module.exports = (function() {
     var _return = {};
 
     _return.get = function(req, res, next) {
-        if (_.includes(req.user._permissions, "root") || _.includes(req.user._permissions, "mali")) {
-            db.mali.find({}, {
-                __v: false
-            }).populate({
-                path: '_owner',
-                select: 'first_name last_name -_id'
-            }).populate({
-                path: '_dispatch',
-                select: '_station -_id'
-            }).lean().exec(function(err, m) {
-                if (err) {
-                    console.log(err);
-                    res.json({
-                        err: err
-                    });
-                } else {
-                    console.log(m.length);
-                    String.sync_for(m.length, function(loop) {
-                        db.dispatch.populate(m[loop.iteration()]._dispatch, {
-                            path: '_station',
-                            select: 'name -_id'
-                        }, function(err, rr) {
-                            if (err) {
-                                console.log(err);
+
+        var _root = _.includes(req.user._permissions, "root"),
+            _see_all_women = _.includes(req.user._permissions, "woman_mali"),
+            _see_all_man = _.includes(req.user._permissions, "man_mali"),
+            _see_all_users = (_see_all_man && _see_all_women);
+
+
+        if ((_see_all_women || _see_all_man || _root)) {
+            if (req.query.from && req.query.to) {
+                var query = {};
+                if (!(_root || _see_all_users)) {
+                    if (_see_all_man)
+                        query.gender = true;
+                    else if (_see_all_women)
+                        query.gender = {
+                            $exists: false
+                        }
+                }
+
+                console.open(query);
+
+                db.mali.find({}, {
+                    __v: false
+                }).populate({
+                    path: '_owner',
+                    select: 'first_name last_name -_id',
+                    match: query
+                }).populate({
+                    path: '_dispatch',
+                    select: '_station -_id'
+                }).lean().exec(function(err, m) {
+                    if (err) {
+                        console.log(err);
+                        res.json({
+                            err: err
+                        });
+                    } else {
+                        String.sync_for(m.length, function(loop) {
+                            console.open(m[loop.iteration()]._owner);
+                            if (m[loop.iteration()]._owner) {
+                                db.dispatch.populate(m[loop.iteration()]._dispatch, {
+                                    path: '_station',
+                                    select: 'name -_id'
+                                }, function(err, rr) {
+                                    if (err) {
+                                        console.log(err);
+                                    } else {
+                                        loop.next();
+                                    }
+                                });
                             } else {
+                                m.splice(loop.iteration(), 1);
+                                loop.minmin();
                                 loop.next();
                             }
+                        }, function() {
+                            res.json(m)
                         });
-                    }, function() {
-                        res.json(m)
-                    });
-                }
+                    }
+                });
+            } else res.json({
+                err: "low args"
             });
-        }
+        } else res.status(403).json({
+            err: "permission denied"
+        });
     };
 
     _return.post = function(req, res, next) {
@@ -68,7 +100,7 @@ module.exports = (function() {
                 // comming soooooooooon
                 next();
             } else res.json({
-                error: "low args"
+                err: "low args"
             });
 
         } else next();
