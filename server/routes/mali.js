cc8cc1fa0f630b4a20be99377492212448f97ffd
elyas ghasemi <egh1374@gmail.com>
2015-08-06 20:33:44 +0430

    new style of speak with client!

--- server/routes/mali.js
+++ server/routes/mali.js
@@ -13,26 +13,24 @@ module.exports = (function() {
 
 
         if ((_see_all_women || _see_all_man || _root)) {
-            if (req.query.from && req.query.to) {
-                var query = {};
-                if (!(_root || _see_all_users)) {
-                    if (_see_all_man)
-                        query.gender = true;
-                    else if (_see_all_women)
-                        query.gender = {
-                            $exists: false
-                        }
-                }
+            var query = {};
+            if (!(_root || _see_all_users)) {
+                if (_see_all_man)
+                    query.gender = true;
+                else if (_see_all_women)
+                    query.gender = {
+                        $exists: false
+                    }
+            }
 
+            if (req.query.type == "list") {
                 db.mali.find({}, {
-                    __v: false
+                    _owner: true,
+                    cost: true
                 }).populate({
                     path: '_owner',
                     select: 'first_name last_name -_id',
                     match: query
-                }).populate({
-                    path: '_dispatch',
-                    select: '_station -_id'
                 }).lean().exec(function(err, m) {
                     if (err) {
                         console.log(err);
@@ -60,13 +58,96 @@ module.exports = (function() {
                                 loop.next();
                             }
                         }, function() {
-                            res.json(m.slice(req.query.from, req.query.to)); // memory leak here!
+                            res.json(m);
                         });
                     }
                 });
-            } else res.json({
-                err: "low args"
-            });
+            } else if (req.query.type == "get_data" && req.query.data && req.query.data._id) {
+                db.mali.findOne({
+                    _id: req.query._id
+                }, {
+                    __v: false
+                }).populate({
+                    path: '_owner',
+                    select: 'first_name last_name -_id',
+                    match: query
+                }).populate({
+                    path: '_dispatch',
+                    select: '_station date -_id',
+                }).lean().exec(function(err, m) {
+                    if (err) {
+                        console.log(err);
+                        res.json({
+                            err: err
+                        });
+                    } else if (m) {
+                        if (m._owner) {
+                            db.dispatch.populate(m._dispatch, {
+                                path: '_station',
+                                select: 'name -_id'
+                            }, function(err, rr) {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    if (m._dispatch.date) {
+                                        Date.en_to_persion_date(m._dispatch.date, function(date) {
+                                            m._dispatch.date = date.date;
+                                        });
+                                    }
+                                    res.json(m);
+                                }
+                            });
+                        } else {
+                            res.status(403).json({
+                                err: "permission denied"
+                            });
+                        }
+                    }
+                });
+            }
+
+            // Old starategy
+
+            // db.mali.find({}, {
+            //     __v: false
+            // }).populate({
+            //     path: '_owner',
+            //     select: 'first_name last_name -_id',
+            //     match: query
+            // }).populate({
+            //     path: '_dispatch',
+            //     select: '_station -_id'
+            // }).lean().exec(function(err, m) {
+            //     if (err) {
+            //         console.log(err);
+            //         res.json({
+            //             err: err
+            //         });
+            //     } else {
+            //         String.sync_for(m.length, function(loop) {
+            //             var i = loop.iteration();
+            //             if (m.length == 0) loop.break(true);
+            //             if (m[i] && m[i]._owner) {
+            //                 db.dispatch.populate(m[i]._dispatch, {
+            //                     path: '_station',
+            //                     select: 'name -_id'
+            //                 }, function(err, rr) {
+            //                     if (err) {
+            //                         console.log(err);
+            //                     } else {
+            //                         loop.next();
+            //                     }
+            //                 });
+            //             } else {
+            //                 m.splice(i, 1);
+            //                 loop.minmin();
+            //                 loop.next();
+            //             }
+            //         }, function() {
+            //             res.json(m.slice(req.query.from, req.query.to)); // memory leak here!
+            //         });
+            //     }
+            // });
         } else res.status(403).json({
             err: "permission denied"
         });
