8fdebe5d2bf3288b8b43c286eab25ba7b8283d12
elyas ghasemi <egh1374@gmail.com>
2015-09-24 11:13:05 +0330

    add /temp_users

--- server/routes/temp_users.js
+++ server/routes/temp_users.js
@@ -8,16 +8,41 @@ module.exports = (function() {
     };
 
     _return.post = function(req, res) {
-        new db.temp_users(req.body).save(function(err) {
-            if (err) {
-                res.json({
-                    err: err
-                });
-            } else {
-                res.redirect('/temp_user');
-                console.log( req.user.first_name + " " + req.user.last_name + " add new temp user!");
-            }
-        });
+        // console.open(req.body);
+        if (req.body && req.body.mobile) {
+            db.temp_users.findOne({
+                mobile: req.body.mobile
+            }, {
+                _id: true
+            }).lean().exec(function(err, _rr) {
+                // console.open(_rr);
+                if (err) {
+                    console.log(err);
+                    res.json({
+                        err: err
+                    });
+                } else if (!_rr) {
+                    new db.temp_users(req.body).save(function(err) {
+                        if (err) {
+                            res.json({
+                                err: err
+                            });
+                        } else {
+                            res.redirect('/temp_users');
+                            console.log(req.user.first_name + " " + req.user.last_name + " add new temp user!");
+                        }
+                    });
+                } else if (_rr) {
+                    // res.send("esists in db!");
+                    res.redirect('/temp_users');
+                    console.log("temp_user phone exist in db.");
+                }
+            });
+        } else {
+            res.json({
+                err: "low args"
+            });
+        }
     };
 
     return _return;
