1919eb74c0fd675240f2ad1c5e41bac668c0f4b9
elyas74 <egh1374@gmail.com>
2015-12-09 16:29:56 +0330

    add to & from to get all data in /remembrance

--- server/routes/remeberance.js
+++ server/routes/remeberance.js
@@ -6,41 +6,52 @@ module.exports = (function() {
 
     _return.get = function(req, res, next) {
         if (req.query && req.query.type == "all") {
-            db.rememberance.find({}, {
-                __v: false
-            }).lean().exec(function(err,
-                rememberances) {
-                if (err) {
-                    console.log(err);
-                    res.status(500).json({
-                        add: false,
-                        err: err
-                    });
-                } else {
-                    res.json(rememberances);
-                }
-            })
-        } else if (req.query && req.query.type == "single" && req.query
-            ._id) {
-            db.rememberance.findOne({
-                _id: req.query
-                    ._id
-            }, {
-                _id: false,
-                __v: false
-            }).lean().exec(function(err,
-                rememberances) {
-                if (err) {
-                    console.log(err);
-                    res.status(500).json({
-                        add: false,
-                        err: err
-                    });
-                } else {
-                    res.json(rememberances);
-                }
-            })
+            var from = req.query.from || 0;
+            var to = req.query.to || 10;
+
+            db.rememberance
+                .find({}, {
+                    __v: false
+                })
+                .skip(from)
+                .limit(to - from)
+                .lean()
+                .exec(function(err,
+                    rememberances) {
+                    if (err) {
+                        console.log(err);
+                        res.status(500).json({
+                            add: false,
+                            err: err
+                        });
+                    } else {
+                        res.json(rememberances);
+                    }
+                })
         }
+        // now this part in useless!
+
+        /*else if (req.query && req.query.type == "single" && req.query
+                   ._id) {
+                   db.rememberance.findOne({
+                       _id: req.query
+                           ._id
+                   }, {
+                       _id: false,
+                       __v: false
+                   }).lean().exec(function(err,
+                       rememberances) {
+                       if (err) {
+                           console.log(err);
+                           res.status(500).json({
+                               add: false,
+                               err: err
+                           });
+                       } else {
+                           res.json(rememberances);
+                       }
+                   })
+               }*/
     };
 
     _return.post = function(req, res, next) {
@@ -55,11 +66,15 @@ module.exports = (function() {
                         err: err
                     });
                 } else {
+                    console.log(req.user.first_name + " " +
+                        req.user.last_name +
+                        " add a new remeberance");
                     res.json({
                         add: true
                     });
                 }
             });
+            // like a remembrane and add it to set of likers
         } else if (req.user && req.body.type == "like" && req.body.data &&
             req.body.data._remeberance_id) {
             db.remeberance.update({
@@ -76,11 +91,15 @@ module.exports = (function() {
                         err: err
                     });
                 } else {
+                    console.log(req.user.first_name + " " +
+                        req.user.last_name +
+                        " like a remeberance");
                     res.json({
                         like: true
                     });
                 }
             });
+            // add comment to a remembrane in it's array
         } else if (req.user && req.body.type == "comment" && req.body
             .data && req.body.data._remeberance_id && req.body.data
             .text) {
@@ -101,6 +120,9 @@ module.exports = (function() {
                         err: err
                     });
                 } else {
+                    console.log(req.user.first_name + " " +
+                        req.user.last_name +
+                        " comment a remeberance");
                     res.json({
                         comment: true
                     });
